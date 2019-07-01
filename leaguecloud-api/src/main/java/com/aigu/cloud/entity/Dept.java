fdcaa0e482e752b78e01c32afb593c317276eb28
jianyuan <18672793611@163.com>
2019-07-01 18:41:11 +0800

    Initial commit

---
+++ leaguecloud-api/src/main/java/com/aigu/cloud/entity/Dept.java
@@ -0,0 +1,22 @@
+package com.aigu.cloud.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Accessors(chain = true)
+public class Dept implements Serializable {
+    private Long deptno;
+
+    private String name;
+
+    private String db_source;
+
+}
